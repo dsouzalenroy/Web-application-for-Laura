dac7a3381851b8c31d376317935b02d0cc0e1a18
jc502179 <39153814+jc502179@users.noreply.github.com>
2020-02-11 11:11:58 +1000

    js for 4 members

---
+++ script.js
@@ -0,0 +1,259 @@
+
+function findCitizenshipTotal(){
+    var arr = document.getElementsByName('citizenshiBehaviour');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('citizenshipTotal').value = tot;
+    }
+
+function findCitizenshipTotalcolumn2(){
+    var arr = document.getElementsByName('citizenshiBehaviour1.1');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('citizenshipTotal1.1').value = tot;
+    }
+
+function findCitizenshipTotalcolumn3(){
+        var arr = document.getElementsByName('citizenshiBehaviour1.2');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('citizenshipTotal1.2').value = tot;
+        }
+
+function findCitizenshipTotalcolumn4(){
+        var arr = document.getElementsByName('citizenshiBehaviour1.3');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('citizenshipTotal1.3').value = tot;
+        }
+
+function findTeamWorkTotal(){
+    var arr = document.getElementsByName('teamwork');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+       if(parseInt(arr[i].value))
+         tot += parseInt(arr[i].value);
+    }
+    document.getElementById('teamworkTotal').value = tot;
+    }
+
+function findTeamWorkTotalColumn2(){
+    var arr = document.getElementsByName('teamwork');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+       if(parseInt(arr[i].value))
+         tot += parseInt(arr[i].value);
+    }
+    document.getElementById('teamworkTotal2').value = tot;
+    }
+
+function findTeamWorkTotalColumn3(){
+    var arr = document.getElementsByName('teamwork');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+       if(parseInt(arr[i].value))
+         tot += parseInt(arr[i].value);
+    }
+    document.getElementById('teamworkTotal3').value = tot;
+    }
+
+function findTeamWorkTotalColumn4(){
+    var arr = document.getElementsByName('teamwork');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+       if(parseInt(arr[i].value))
+         tot += parseInt(arr[i].value);
+    }
+    document.getElementById('teamworkTotal4').value = tot;
+    }
+
+function findLeadershipTotal(){
+    var arr = document.getElementsByName('leadership');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('leadershipTotal').value = tot;
+    }
+
+function findLeadershipTotalColumn2(){
+    var arr = document.getElementsByName('leadership');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('leadershipTotal2').value = tot;
+    }
+
+function findLeadershipTotalColumn3(){
+    var arr = document.getElementsByName('leadership');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('leadershipTotal3').value = tot;
+    }
+
+function findLeadershipTotalColumn4(){
+    var arr = document.getElementsByName('leadership');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('leadershipTotal4').value = tot;
+    }
+
+function findTechCompTotal(){
+    var arr = document.getElementsByName('techComp');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('techCompTotal').value = tot;
+    }
+
+function findTechCompTotalColumn2(){
+        var arr = document.getElementsByName('techComp');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('techCompTotal2').value = tot;
+        }
+function findTechCompTotalColumn3(){
+        var arr = document.getElementsByName('techComp');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('techCompTotal3').value = tot;
+        }
+function findTechCompTotalColumn4(){
+        var arr = document.getElementsByName('techComp');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('techCompTotal4').value = tot;
+        }
+
+function findWorkProductTotal(){
+    var arr = document.getElementsByName('workProduct');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('workProductTotal').value = tot;
+    }
+
+function findWorkProductTotalColumn2(){
+    var arr = document.getElementsByName('workProduct');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+        tot += parseInt(arr[i].value);
+    }
+    document.getElementById('workProductTotal2').value = tot;
+    }
+
+function findWorkProductTotalColumn3(){
+        var arr = document.getElementsByName('workProduct');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('workProductTotal3').value = tot;
+        }
+
+function findWorkProductTotalColumn4(){
+        var arr = document.getElementsByName('workProduct');
+        var tot=0;
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+        }
+        document.getElementById('workProductTotal4').value = tot;
+        }
+
+//function subtract(){
+//  var Name = ["Name1","Name2","Name3", "Name4"]
+//  var Name1 = document.getElementsByName('Name1').value;
+ // var Name2 = document.getElementsByName('Name2').value;
+ // var Name3 = document.getElementsByName('Name3').value;
+ // var Name4 = document.getElementsByName('Name4').value;
+ // var total = 100;
+ // for(var i=0;i<Name.length;i++){
+ //   if(Name.length < 4 && parseInt(arr[i].value)){
+ //     total = +100 - +parseInt(arr[i].value)
+ //   }
+ // }
+ // document.getElementById('Name1||Name2||Name3||Name4').value = total;
+
+ // }
+  
+function countColumnsFilled(){
+	var Name = parseFloat(document.getElementById('Name').value);
+	
+	var check = 0;
+	var total = 0;
+	if(Name > 0 && Name < 100 && Name !== 'NaN') {
+		check += 1;
+		total += Name;
+	}
+	
+	return [check, total];
+}
+  
+  function fillLastColumn(){
+	var Name = parseFloat(document.getElementById('Name').value);
+	
+	var check_total = countColumnsFilled();
+	if (check_total[0] == 3){
+		if(isNaN(Name) || Name == "") {
+			document.getElementById('Name1').value = (100 - Name).toFixed(2);	
+	
+	}
+  }
+  
+  function checkError1(){
+	var Name = parseFloat(document.getElementById('Name').value);
+	
+	var check_total = countColumnsFilled();
+	if(Name1 <= 0 || Name >= 100 || isNaN(Name)) {
+		document.getElementById('Name').value = "";
+		alert("The value needs to be a number between 0 and 100");
+	}
+	else if((check_total[0]==4 && check_total[1]!==100) || (check_total[0]!==4 && check_total[1]>=100)){
+		document.getElementById('Name').value = "";
+		alert("The total must add up to 100");
+	}
+	else{
+		fillLastColumn();
+	}
+  }
+ 
+  
+
+   
